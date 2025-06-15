c4e3c97beecd378ff967046f68f4dd7027fb0165
wonjong <lej6790@gmail.com>
2025-06-15 11:20:13 +0900

    1. NewsView styling

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <p v-for="item in fetchedNews">
-      <a :href="item.url" target="_blank">
-        {{ item.title }}
-      </a>
-      <small>
-        {{ item.time_ago }} by 
-        <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
-      </small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in fetchedNews" class="post">
+        <!-- 포인트 영역 -->
+        <div class="point">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 역역 -->
+        <div>
+          <p class="news-title">
+            <a :href="item.url" target="_blank">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link :to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>            
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -24,6 +35,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.point {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
