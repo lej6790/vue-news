fc4ff8f283f2a4a61c627898ea962c4386eb5f80
lej9471 <lej9471@gmail.com>
2025-06-16 14:07:11 +0900

    1. JobView styling

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    <p v-for="item in fetchedAsk">
-      <router-link :to="`/item/${item.id}`">
-        {{ item.title }}
-      </router-link>
-      <small>{{ item.time_ago }} by {{ item.user }}</small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in fetchedAsk" class="post">
+        <!-- 포인트 영역 -->
+        <div class="point">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 역역 -->
+        <div>
+          <p class="news-title">
+            <router-link :to="`/item/${item.id}`">
+              {{ item.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <router-link :to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>            
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -22,5 +36,28 @@ export default {
 </script>
 
 <style>
-
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.point {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
