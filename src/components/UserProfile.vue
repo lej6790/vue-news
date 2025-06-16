8dbdd2952697239a85c423fbcdf191ce3bdae46b
wonjong <lej6790@gmail.com>
2025-06-16 22:16:53 +0900

    1. Implemented UserProfile component

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="user-container">
+        <div>
+            <i class="fa-solid fa-user"></i>
+        </div>
+        <div class="user-description">
+            <div>{{ info.id }}</div>
+            <div class="time">
+                {{ info.created }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        info: Object
+    }
+}
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5em;
+}
+.fa-user {
+  font-size: 2.5em;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7em;
+}
+</style>
